7cecd790452007ee40ac27e9736f854a4420105e
yanshi <19fi065@gmail.com>
2021-11-30 02:30:35 +0900

    ダイアログの進捗

--- Game_Dialog_MusicInfo.cpp
+++ Game_Dialog_MusicInfo.cpp
@@ -12,15 +12,28 @@ INT_PTR CALLBACK Game::Dialog::MusicInfoDialogProc(HWND hWnd, UINT uMsg, WPARAM 
 			gfb.openExplorer(title, filter, filePath);
 			SetDlgItemText(hWnd, IDC_EDITFilePath, filePath);
 			break;
+		case IDOK:
+			isShowMusicInfoDlg = false;
+			EndDialog(hWnd,NULL);
+			break;
+		case IDCANCEL:
+			isShowMusicInfoDlg = false;
+			EndDialog(hWnd, NULL);
+			break;
 		}
 		break;
 	case WM_CLOSE:
-
+		isShowMusicInfoDlg = false;
+		EndDialog(hWnd, NULL);
 	}
 	return 0;
 }
 
-void Game::Dialog::Game_Dialog_MusicInfo::getMusicInfoFromDlg(char(&filePath)[MAX_PATH],std::uint8_t& bpm, double& totalMinutes, double& beginDlay) {
+Game::Dialog::Game_Dialog_MusicInfo::Game_Dialog_MusicInfo() : MINUTE(60.0),BPMCHARMAX(3),TOTALMINUTESCHARMAX(4),BEGINDELAYCHARMAX(2){
+
+}
+
+void Game::Dialog::Game_Dialog_MusicInfo::getMusicInfoFromDlg(char(&filePath)[MAX_PATH], std::uint8_t& bpm, double& totalMinutes, double& beginDelay) {
 	HWND hMainWnd = nullptr;
 	HWND hDialogWnd = nullptr;
 	HINSTANCE hInstance = nullptr;
@@ -31,16 +44,31 @@ void Game::Dialog::Game_Dialog_MusicInfo::getMusicInfoFromDlg(char(&filePath)[MA
 	//ダイアログの作成
 	hDialogWnd = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_MUSICINFO), hMainWnd, MusicInfoDialogProc);
 
+	//各項目の説明の設定
 	SetDlgItemText(hDialogWnd, IDC_BUTTONFilePath, "ファイル選択");
 	SetDlgItemText(hDialogWnd, IDC_STATICBPM, "BPM");
-	SetDlgItemText(hDialogWnd, IDC_STATICTotalMinutes, "再生時間（3分35秒→335");
-	SetDlgItemText(hDialogWnd, IDC_STATICBeginDelay, "曲が流れるまでのズレ");
+	SetDlgItemText(hDialogWnd, IDC_STATICTotalMinutes, "再生時間（秒)");
+	SetDlgItemText(hDialogWnd, IDC_STATICBeginDelay, "曲が流れ始めるまでの時間(秒)");
+
+	//EDIITBOXの文字数制限
+	SendMessage(GetDlgItem(hDialogWnd, IDC_EDITBPM), EM_SETLIMITTEXT, BPMCHARMAX, NULL);
+	SendMessage(GetDlgItem(hDialogWnd, IDC_EDITTotalMinutes), EM_SETLIMITTEXT, TOTALMINUTESCHARMAX, NULL);
+	SendMessage(GetDlgItem(hDialogWnd, IDC_EDITBeginDelay), EM_SETLIMITTEXT, BEGINDELAYCHARMAX, NULL);
 
 	//ダイアログをDXライブラリに登録(DXライブラリは単一のダイアログのみ対応)
 	SetDialogBoxHandle(hDialogWnd);
 
-	while (isShowDlg) {
-		Sleep(1000);
+	ShowWindow(hDialogWnd, SW_SHOW);
+
+	while (isShowMusicInfoDlg) {
+		//ダイアログボックスの入力が終わるまで待つ
+		ProcessMessage();
+		ClearDrawScreen();
+		ScreenFlip();
 	}
-	GetDlgItemText(hDialogWnd, IDC_EDDITFilePath,filePath,MAX_PATH);
+
+	GetDlgItemText(hDialogWnd, IDC_EDDITFilePath, filePath, MAX_PATH);
+	bpm=GetDlgItemText(hDialogWnd,IDC_EDITBPM,NULL,true);
+	totalMinutes=GetDlgItemText(hDialogWnd, IDC_EDITTotalMinutes, NULL, true)/MINUTE;
+	beginDelay=GetDlgItemText(hDialogWnd, IDC_EDITBeginDelay, NULL, true);
 }
--- Game_Dialog_MusicInfo.h
+++ Game_Dialog_MusicInfo.h
@@ -8,14 +8,18 @@
 
 namespace Game {
 	namespace Dialog {
-		boolean isShowDlg = TRUE;
+		boolean isShowMusicInfoDlg = true;
 		INT_PTR CALLBACK MusicInfoDialogProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 		class Game_Dialog_MusicInfo
 		{
 		private:
-
+			const double MINUTE;
+			const std::uint8_t BPMCHARMAX;
+			const std::uint8_t TOTALMINUTESCHARMAX;
+			const std::uint8_t BEGINDELAYCHARMAX;
 		public:
-			void getMusicInfoFromDlg(char(&filePath)[MAX_PATH],std::uint8_t& bpm, double& totalMinutes, double& beginDlay);
+			Game_Dialog_MusicInfo();
+			void getMusicInfoFromDlg(char(&filePath)[MAX_PATH],std::uint8_t& bpm, double& totalMinutes, double& beginDelay);
 		};
 	}
 }
